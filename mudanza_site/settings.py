cb7fa6e136be505607970d8422f84a69fd477d25
Daniel Henriquez <daniel.henriquez.v@hotmail.com>
2025-12-02 11:11:00 +0100

    statics

--- mudanza_site/settings.py
+++ mudanza_site/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
